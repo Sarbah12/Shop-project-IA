832d58163a5b0ca866f64723742b1508e434ed59
Sarbah12 <85554254+Sarbah12@users.noreply.github.com>
2021-11-17 22:18:03 +0000

    having issues with app.js

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import signup from "./src/screens/signup.js";
+import login from "./src/screens/login.js";
+import landing_page from "./src/screens/landing_page.js";
+import settings from "./src/screens/settings.js";
+import logout from "./src/screens/logout.js";
+import cart from "./src/screens/cart.js";
 
-const Stack = createStackNavigator();
-import HomeScreen from './src/view/screens/Home';
-import DetailsSceeen from './src/view/screens/Details';
-import {StatusBar} from 'react-native';
-import COLORS from './src/consts/colors';
+const Stack = createNativeStackNavigator();
 
-const App = () => {
+export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="dark-content" backgroundColor={COLORS.white} />
-      <Stack.Navigator screenOptions={{header: () => null}}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={Details} />
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="cart" component={cart}/>
+        <Stack.Screen name="landing_page" component={landing_page} />
+        <Stack.Screen name="logout" component={logout} />       
+        <Stack.Screen name="settings" component={settings} />
+        <Stack.Screen name="login" component={login} />
+        <Stack.Screen name="signup" component={signup} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
-
-export default App;
+}
